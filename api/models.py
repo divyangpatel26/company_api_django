09a178cbc9bc546e2d7ca4bda50e0c41e1aef7b1
Divyangkumar Patel <41123607+divyangpatel26@users.noreply.github.com>
2023-10-05 15:55:45 +0100

    companies model created

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Company(models.Model):
+    comapny_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    about = models.TextField()
+    type = models.CharField(max_length=100,
+                            choices=(('it', 'it'), ('non-it', 'non-it'), ('Mobile Phones', 'Mobile Phones')))
+    added_date = models.DateField(auto_now=True)
+    active=models.BooleanField(default=True)
